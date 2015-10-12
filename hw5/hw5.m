22613c8a17c503eff85e07dd952acb5889abf763
Steven Macenski <smacens2@TL-321D-11.ad.uillinois.edu>
2015-10-12 13:50:02 -0500

    minor fixes

--- hw5/hw5.m
+++ hw5/hw5.m
@@ -343,7 +343,11 @@ t4 = 0; t5 = 0; s4 = 0; s5 = 0;
     
     s4 = [1 0; 0 0; 0 1];
     s5 = [1 0; 0 1; 0 0];
-    
+      w_34in4 = zeros(3,1);
+      w_04in4 = zeros(3,1);
+      w_35in5 = zeros(3,1);
+      w_05in5 = zeros(3,1);
+
  h = [zeros(3,1);
       -m4*R_3in0*wedge(w_03in3)*wedge(w_03in3)*robot.o_4in3;
       -m5*R_3in0*wedge(w_03in3)*wedge(w_03in3)*robot.o_5in3;
@@ -353,7 +357,7 @@ t4 = 0; t5 = 0; s4 = 0; s5 = 0;
   
   size(h)
 
-  
+
   %F = TODO typing in
   F =   [m*eye(3) zeros(3) zeros(3,1) zeros(3,1) R_3in0 R_3in0 zeros(3,2) zeros(3,2);...
         m4*eye(3) -m4*R_3in0*wedge(robot.o_4in3) zeros(3,1) zeros(3,1) -R_3in0 zeros(3) zeros(3,2) zeros(3,2);
